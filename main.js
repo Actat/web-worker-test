1e3d4bb37e5d9b58097915d3c42933dfdfb2b1b8
Actat <akio.toyosh@gmail.com>
2022-07-06 14:30:16 +0900

    with copy

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = async function() {
   console.log("window loaded");
 
   if (!window.Worker) {
@@ -9,18 +9,29 @@ window.onload = function() {
   console.log("web worker is available");
 
   var d = new Date();
-  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "] start worker1");
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][main] start worker1");
   var worker1 = new Worker("worker1.js");
 
   worker1.onmessage = function(e) {
     var d = new Date();
-    console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "] response received from worker1", e);
+    console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][main] response received from worker1", e);
   }
 
+  var ui8a = new Uint8Array(1024 * 1024 * 32);
+  for (var i = 0; i < ui8a.length; i++ ) {
+    ui8a[i] = 0;
+  }
+
+  var d = new Date();
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][main] sleep 1 sec");
+  await new Promise(s => {setTimeout(s, 1000)})
+  var d = new Date();
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][main] wake up");
+
   d = new Date();
-  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "] post test message to worker1");
-  worker1.postMessage("test message");
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][main] post test message to worker1");
+  worker1.postMessage(ui8a.buffer);
   d = new Date();
-  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "] test message to worker1 has posted");
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][main] test message to worker1 has posted");
 
 }
--- worker1.js
+++ worker1.js
@@ -1,11 +1,13 @@
 var d = new Date();
-console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "] worker1.js is called");
+console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][worker1] worker1.js is called");
 
 onmessage = function(e) {
   d = new Date();
-  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "] worker1 receive message: ", e);
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][worker1] worker1 receive message: ", e);
 
   d = new Date();
-  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "] worker1 return response");
-  postMessage("response");
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][worker1] worker1 return response");
+  postMessage(e.data);
+  d = new Date();
+  console.log("[" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds() + "][worker1] worker1 has returned response");
 }
